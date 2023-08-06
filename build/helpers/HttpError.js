3da98537455a1cfbd3340d5aa5eb3eaa2dd9b0e8
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-08-06 20:44:33 +0300

    remove build

--- build/helpers/HttpError.js
+++
@@ -1,8 +0,0 @@
-const messages = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",
-    404: "Not Found",
-};
-export const HttpError = (status, message = messages[status]) => { return { status, message }; };
-//# sourceMappingURL=HttpError.js.map
--- build/types/typescriptTypes.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=typescriptTypes.js.map
